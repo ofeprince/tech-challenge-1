89d3fb719d69b4b980a7023f40af63dffd165b70
Fellipe Prince <lfellipe.prince@gmail.com>
2026-01-20 01:25:52 -0300

    Testing temp folder vercel

--- src/api/config.py
+++ src/api/config.py
@@ -1,14 +1,18 @@
 import os
 from dotenv import load_dotenv
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-DATA_DIR = os.path.join(BASE_DIR, "data")
-os.makedirs(DATA_DIR, exist_ok=True)
-
-DATA_DIR = os.path.join(DATA_DIR, "db")
-os.makedirs(DATA_DIR, exist_ok=True)
-
 load_dotenv()
+
+if os.environ.get('VERCEL'):
+    DB_PATH = '/tmp'
+else:
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    DATA_DIR = os.path.join(BASE_DIR, "data")
+    os.makedirs(DATA_DIR, exist_ok=True)
+
+    DATA_DIR = os.path.join(DATA_DIR, "db")
+    os.makedirs(DATA_DIR, exist_ok=True)
+    DB_PATH = DATA_DIR
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
@@ -17,7 +21,7 @@ class Config:
         'title': os.getenv('SWAGGER_TITLE'),
         'uiversion': 3
     }
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(DATA_DIR, "booksraping.db")}"
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(DB_PATH, 'booksraping.db')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET')
     ADMIN_USER = os.getenv('USER_ADMIN')
